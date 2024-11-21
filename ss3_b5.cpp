36c51cc33598e6a1f0983aa8fae16a16d6f30863
Huidinne <quanghuy432006@gmail.com>
2024-11-21 21:42:14 +0700

    Upload btvn Session3_b5

---
+++ ss3_b5.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	printf("%50s\n", "DANH SACH SINH VIEN");
+	printf("+-----+-----------------+------+---------------+---------------------------+\n");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "STT", "Ho va ten", "Tuoi", "So dien thoai", "Email");
+	printf("----------------------------------------------------------------------------\n");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "1", "Nguyen Van A", "20", "0904488481", "anv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "2", "Nguyen Van B", "21", "0904488482", "bnv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "3", "Nguyen Van C", "23", "0904488483", "cnv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "4", "Nguyen Van D", "24", "0904488484", "dnv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "5", "Nguyen Van E", "25", "0904488485", "env@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "6", "Nguyen Van F", "26", "0904488486", "fnv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "7", "Nguyen Van G", "27", "0904488487", "gnv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "8", "Nguyen Van H", "28", "0904488488", "hnv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "9", "Nguyen Van I", "29", "0904488489", "inv@rikkeiacademy.com");
+	printf("| %-3s | %-15s | %-4s | %-13s | %-25s |\n", "10", "Nguyen Van K", "30", "0904488480", "knv@rikkeiacademy.com");
+	printf("+--------------------------------------------------------------------------+\n");
+}
+    
